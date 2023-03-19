718bdb4496a3f2e270782bf15bf80f8ad0b4eca4
Theohorsmeier <theohorsmeier@gmail.com>
2023-03-19 16:52:01 +0100

    Basic palettes working, first params

--- project/src/index.js
+++ project/src/index.js
@@ -1,105 +1,220 @@
-console.log(fxhash)
-console.log(fxrand())
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as helper from "@liamegan1/fxhash-helpers"
 
-const sp = new URLSearchParams(window.location.search);
-console.log(sp);
+/**
+ * Palettes
+ */
+const palettes = [  
+  {    
+    name: "Kerk",
+    colors: [ 0xaaaaaa, 0x0059ff, 0xff5000, 0xffffff, 0x000000 ]
+  },
+  {
+    name: "Zondag",
+    colors: [ 0xaaaaaa, 0x00ffea, 0xce00ff, 0xffffff, 0x000000 ]
+  },
+  {
+    name: "Ontvreemd",
+    colors: [ 0x00adff, 0xff001a, 0xaaaaaa, 0xffffff, 0x000000 ]
+  },
+  {
+    name: "Analogous",
+    colors: [ 0xFFC300, 0xFF5733, 0xC70039, 0x900C3F, 0x581845 ]
+  },
+  {
+    name: "Complementary",
+    colors: [ 0xFF4136, 0x0074D9, 0xFFDC00, 0x001f3f, 0x3D9970 ]
+  },
+  {
+    name: "Monochromatic",
+    colors: [ 0xF7F7F7, 0xD9D9D9, 0xA7A7A7, 0x737373, 0x404040 ]
+  }
+];
 
-// this is how to define parameters
+const paletteNames = palettes.map(p => p.name)
+
+/**
+ * Params
+ * - Palette
+ * - Wrap ranges
+ * - Total Lines
+ * - Total Particles (max 100.000)
+ * - Sections (min 2, max 8?)
+ * - Sprayfactor
+ * - Linefactor
+ *  
+ * Features not params:
+ * - rotation direction?(xyz increments)
+ */
+
 $fx.params([
   {
-    id: "number_id",
-    name: "A number/float64",
+    id: "particleCount",
+    name: "Particles",
     type: "number",
-    //default: Math.PI,
-    options: {
-      min: 1,
-      max: 10,
-      step: 0.00000000000001,
-    },
+    default: 1000000,
+    options: { min: 1000,max: 10000000,step: 1000 },
   },
   {
-    id: "bigint_id",
-    name: "A bigint",
-    type: "bigint",
-    //default: BigInt(Number.MAX_SAFE_INTEGER * 2),
-    options: {
-      min: Number.MIN_SAFE_INTEGER * 4,
-      max: Number.MAX_SAFE_INTEGER * 4,
-      step: 1,
-    },
+    id: "curveCount",
+    name: "Curves",
+    type: "number",
+    default: 100,
+    options: { min: 20,max: 1000,step: 1 },
   },
   {
-    id: "select_id",
-    name: "A selection",
+    id: "wrapMinStart",
+    name: "wrapMinStart",
+    type: "number",
+    default: 0,
+    options: { min: -20,max: 20,step: 0.1 },
+  },
+  {
+    id: "wrapMinEnd",
+    name: "wrapMinEnd",
+    type: "number",
+    default: 0,
+    options: { min: -20,max: 20,step: 0.1 },
+  },
+  {
+    id: "wrapMaxStart",
+    name: "wrapMaxStart",
+    type: "number",
+    default: 10,
+    options: { min: -20,max: 50,step: 0.1 },
+  },
+  {
+    id: "wrapMaxEnd",
+    name: "wrapMaxEnd",
+    type: "number",
+    default: 20,
+    options: { min: -20,max: 50,step: 0.1 },
+  },
+  {
+    id: "wrapFactor",
+    name: "wrapFactor",
+    type: "number",
+    default: 1.0,
+    options: { min: 0.2,max: 1.0,step: 0.01 },
+  },
+  {
+    id: "palette_id",
+    name: "Palette",
     type: "select",
-    //default: "pear",
     options: {
-      options: ["apple", "orange", "pear"],
+      options: paletteNames,
     }
-  },
-  {
-    id: "color_id",
-    name: "A color",
-    type: "color",
-    //default: "ff0000",
-  },
-  {
-    id: "boolean_id",
-    name: "A boolean",
-    type: "boolean",
-    //default: true,
-  },
-  {
-    id: "string_id",
-    name: "A string",
-    type: "string",
-    //default: "hello",
-    options: {
-      minLength: 1,
-      maxLength: 64
-    }
-  },
-]);
+  }
+])
 
-// this is how features can be defined
+
 $fx.features({
-  "A random feature": Math.floor($fx.rand() * 10),
-  "A random boolean": $fx.rand() > 0.5,
-  "A random string": ["A", "B", "C", "D"].at(Math.floor($fx.rand()*4)),
-  "Feature from params, its a number": $fx.getParam("number_id"),
+  "Palette": $fx.getParam("palette_id"),
 })
 
-// log the parameters, for debugging purposes, artists won't have to do that
-console.log("Current param values:")
-// Raw deserialize param values 
-console.log($fx.getRawParams())
-// Added addtional transformation to the parameter for easier usage
-// e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0] 
-console.log($fx.getParams())
+/**
+ * Do I want these to be params too????
+ */
 
-// how to read a single raw parameter
-console.log("Single raw value:")
-console.log($fx.getRawParam("color_id"));
-// how to read a single transformed parameter
-console.log("Single transformed value:")
-console.log($fx.getParam("color_id"));
+const particleSmallSpread = 0.1
+const particleLargeSpread = 1.5
+const bezierRadius = 10
+const bezierControlRadius = 10
+const particleSmallPower = 5
+const particleLargePower = 2
 
-// update the document based on the parameters
-document.body.style.background = $fx.getParam("color_id").hex.rgba
-document.body.innerHTML = `
-<p>
-url: ${window.location.href}
-</p>
-<p>
-hash: ${$fx.hash}
-</p>
-<p>
-params:
-</p>
-<pre>
-${$fx.stringifyParams($fx.getRawParams())}
-</pre>
-<pre style="color: white;">
-${$fx.stringifyParams($fx.getRawParams())}
-</pre>
-`
+const offsetLerpFactor = helper.FXRandomBetween(0,1) 
+const lineLerpFactor = helper.FXRandomBetween(0,1)
+
+/**
+ * Setup the scene
+*/
+const pallette_index = paletteNames.indexOf($fx.getParam("palette_id"))
+
+const canvas = document.querySelector('canvas.webgl')
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(5,5,5)
+const meshes = []
+for (let index = 0; index < palettes[pallette_index].colors.length; index++) {
+  const material = new THREE.MeshBasicMaterial({color: palettes[pallette_index].colors[index]})
+  const mesh = new THREE.Mesh(geometry,material)
+  mesh.position.x = index*5
+  meshes.push(mesh)
+
+  scene.add(mesh)
+  
+}
+
+
+
+
+const sizes = {
+  width       : window.innerWidth,
+  height      : window.innerHeight,
+  pixelRatio : Math.min(window.devicePixelRatio, 2)
+}
+
+window.addEventListener('resize', () =>
+{
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+const camera = new THREE.PerspectiveCamera(
+  30, 
+  sizes.width / sizes.height, 
+  0.1, 
+  1000
+)
+
+camera.position.set(50, 50, 50)
+camera.lookAt(new THREE.Vector3(0,0,0))
+scene.add(camera)
+
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 0.2 * ( helper.FXRandomBool ? 1 : -1)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // mesh.rotation.z += 0.01
+
+
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
